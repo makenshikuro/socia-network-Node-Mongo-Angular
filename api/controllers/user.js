7eb42614face080a4d76af2db78254b95aa10374
Ubusuts <adbuspaz@gmail.com>
2018-08-29 20:19:04 +0200

    Add Upload feature

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,8 +1,11 @@
 'use strict'
 var bcrypt = require ('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
+var fs = require('fs');
 
 var User = require('../models/user');
-
+var jwt = require('../services/jwt');
+var path = require('path');
 
 function home (req,res){
 	res.status(200).send({
@@ -66,8 +69,178 @@ function saveUser(req, res){
 	}
 }
 
+function loginUser(req, res){
+	var params = req.body;
+
+	var email = params.email;
+	var password = params.password;
+
+	User.findOne({email: email}, (err, user) =>{
+		if(err) return res.status(500).send({message: 'Error en la petición'});
+		if (user){
+			bcrypt.compare(password, user.password, (err, check) => {
+				if(check){
+					//Devolver datos de usuario
+					if (params.gettoken){
+						//generar y Devolver Token
+						return res.status(200).send({
+							token: jwt.createToken(user)
+						});
+					}else{
+						user.password = undefined;
+						return res.status(200).send({user})
+					}
+
+					
+
+				}else{
+					return res.status(404).send({message: 'El usuario no se ha podido identificar'});
+				}
+			});
+		}else{
+			return res.status(404).send({message: 'El usuario no se ha podido identificar'});
+		}
+	});
+}
+// Conseguir Datos de un usuario
+function getUser(req, res) {
+	var userId = req.params.id;
+	User.findById(userId, (err,user) => {
+		if (err){
+			return res.status(500).send({
+				message: 'Error en la petición'
+			});
+		}
+		if (!user){
+			return res.status(404).send({message: 'El usuario no existe'});
+		}
+
+		user.password = undefined;
+		return res.status(200).send({user});
+	});
+}
+// Devolver listado de users paginados
+
+function getUsers(req, res){
+	var identity_user_id = req.user.sub;
+	var page = 1;
+	if(req.params.page){
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 5;
+
+	User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+		if(err) return res.status(500).send({message: 'Error en la petición'});
+
+		if (!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
+
+		return res.status(200).send({
+			users,
+			total,
+			pages: Math.ceil(total/itemsPerPage)
+		});
+
+
+	});
+}
+
+function updateUser(req, res) {
+	var userId = req.params.id;
+
+	var update = req.body;
+
+	// borrar propiedad password
+	delete update.password;
+
+	if(userId != req.user.sub){
+
+		return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+	}
+
+	User.findByIdAndUpdate(userId, update, {new:true}, (err, userUpdated) => {
+		if(err) return res.status(500).send({message: 'Error en la petición'});
+
+		if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+		return res.status(200).send({user: userUpdated});
+	});
+
+}
+//Subir archivos de imagen/avatar de usuario
+function uploadImage(req, res) {
+	var userId = req.params.id;
+
+	
+
+	if(req.files){
+		var file_path = req.files.image.path;
+		console.log(file_path);
+
+		var file_split = file_path.split('\\');
+		console.log(file_split);
+
+		var file_name = file_split[2];
+		console.log(file_name);
+
+		var ext_split = file_name.split('\.');
+		console.log(ext_split);
+
+		var file_ext = ext_split[1];
+		console.log(file_ext);
+
+		if(userId != req.user.sub){
+			return removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar los datos del usuario');
+
+		}
+
+
+		if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+			//Actualizar documento de usuario logueado
+			User.findByIdAndUpdate(userId, {image: file_name}, {new:true}, (err, userUpdated) =>{
+				if(err) return res.status(500).send({message: 'Error en la petición'});
+
+				if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+				return res.status(200).send({user: userUpdated});
+			});
+		}else{
+			return removeFilesOfUploads(res,file_path, 'Extensión no válida');
+		}
+
+	}else{
+		return res.status(200).send({message:'No se ha enviado archivo'});
+	}
+}
+
+function removeFilesOfUploads(res,file_path, message){
+	fs.unlink(file_path, (err) =>{
+		return res.status(200).send({message: message});
+	});
+}
+
+function getImageFile(req,res) {
+	var image_file = req.params.imageFile;
+
+	var path_file = './uploads/users/'+image_file;
+
+	fs.exists(path_file, (exists) => {
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message:'No existe la imagen...'});
+		}
+	});
+}
+
 module.exports = {
 	home,
 	pruebas,
-	saveUser
+	saveUser,
+	loginUser,
+	getUser,
+	getUsers,
+	updateUser,
+	uploadImage,
+	getImageFile
 }
